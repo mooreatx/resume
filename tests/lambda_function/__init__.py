46daf957c92d69cc54616d8ea9a71e1b61813227
Shawn Moore <shawn.moore7395@gmail.com>
2022-03-07 17:15:47 -0600

    Delete tests/lambda_function directory

--- tests/lambda_function/__init__.py
+++
@@ -1,9 +0,0 @@
-import json
-import boto3
-import os
-
-# Initialize dynamodb boto3 object
-dynamodb = boto3.resource('dynamodb')
-# Set dynamodb table name variable from env
-#ddbTableName = os.environ['databaseName']
-ddbTableName = 'databaseName'
